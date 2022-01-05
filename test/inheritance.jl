cb65e56f0492215463867141d61eac25364da140
t-bltg <tf.bltg@gmail.com>
2022-01-05 18:13:10 +0100

    handle dot expr head specially (#19)
    Treat `.` expressions as syntax rather than equivalent to a
    `getproperty` call. This is a minor breaking change if someone was
    previously relying on it being treated as a call-expr, as they must now
    rewrite that as an explicit call to `getfield`. However, I think, on
    balance, that syntax is likely to be clearer at highlighting the user's
    likely intent.

--- test/inheritance.jl
+++ test/inheritance.jl
@@ -16,6 +16,13 @@ end
     x::Base.promote_op(+, Int, Float64)
 end
 
+struct Foo{T} x::T end
+
+@computed struct ParametricRef{T}
+    x::Base.RefValue{Foo{T}}
+    ParametricRef(x::Foo{T}) where T = new{T}(Ref(x))
+end
+
 @testset "inheritance" begin
     @test isa(@inferred(Parametric{Int}(1)), Parametric{Int, Float64})
     @test isa(Parametric{Int}(1), Bar)
@@ -26,7 +33,8 @@ end
 
     @test isa(@inferred(NonParametricNoParent(1)), NonParametricNoParent{Float64})
     @test !isa(NonParametricNoParent(1), Bar)
+
+    @test isa(@inferred(ParametricRef(Foo(1.0))), ParametricRef{Float64})
+    @test isa(ParametricRef(Foo(1)).x[], Foo{Int})
 end
 end
-
-
